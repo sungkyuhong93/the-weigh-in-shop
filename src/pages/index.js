390fbeed67ff72521deabc0f6feeab5aa285a7ca
Sung Kyu Hong <sungkyuhong93@gmail.com>
2020-02-14 00:15:31 -0500

    added hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title='the weigh in shop'
+      styleClass="default-background"
+    />
   </Layout>
 )
+
+export const query = graphql`
+{
+  img: file(relativePath: {eq:"hero.jpg"}) {
+    childImageSharp {
+      fluid {
+        ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+}
+`
 
 export default IndexPage
